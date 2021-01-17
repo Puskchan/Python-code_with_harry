5680e1a07ef87746a7a7d403e2ab361982f71acf
Aditya Chandhoke <50488410+Puskchan@users.noreply.github.com>
2021-01-17 14:31:32 +0530

    Add files via upload

---
+++ chap_4/1.py
@@ -0,0 +1,10 @@
+b1 = input("Enter fruit 1: ")
+b2 = input("Enter fruit 2: ")
+b3 = input("Enter fruit 3: ")
+b4 = input("Enter fruit 4: ")
+b5 = input("Enter fruit 5: ")
+b6 = input("Enter fruit 6: ")
+b7 = input("Enter fruit 7: ")
+a=[b1,b2,b3,b4,b5,b6,b7]
+print(a)
+
---
+++ chap_4/2.py
@@ -0,0 +1,9 @@
+m1 =  int(input("Enter marks of student 1: "))
+m2 =  int(input("Enter marks of student 2: "))
+m3 =  int(input("Enter marks of student 3: "))
+m4 =  int(input("Enter marks of student 4: "))
+m5 =  int(input("Enter marks of student 5: "))
+m6 =  int(input("Enter marks of student 6: "))
+a = [m1,m2,m3,m4,m5,m6]
+a.sort()
+print(a)
